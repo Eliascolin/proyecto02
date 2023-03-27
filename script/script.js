63d5ff59c7061caffff0c2679c865696ceaa563c
Eliascolin <eliascolina.c@gmail.com>
2023-03-27 17:25:43 -0300

    adicinando imagens

--- script/script.js
+++ script/script.js
@@ -1,15 +1,25 @@
-function hora(){
+function horas(){
 let img = window.document.querySelector(".imagem")
 let mensagem = window.document.querySelector(".mensagem")
 let hora = new Date
-let horaNumero = hora.getHours()
+let horaNumero =  hora.getHours()
 
 if ( horaNumero >= 0 && horaNumero < 12){
-mensagem.innerHTML = `Bom dia agora São  ${hora}`
+mensagem.innerHTML = `Bom dia agora São  ${horaNumero}  horas`
 img.style.backgroundImage="url(/img/amanhecer.jpg)"
+document.body.style.background = '#f5dd83'
+} 
+else if (horaNumero >= 12 && horaNumero < 18){
+    mensagem.innerHTML = `Boa tarde  agora São ${horaNumero} horas`
+    img.src= './img/atardecer.jpg'
+    document.body.style.background = '#7b7e85'
+}
+else{mensagem.innerHTML = `Boa noite agora São  ${horaNumero} horas`
+img.src= './img/noite.jpg'
+document.body.style.background = '#223a56'}
 }
 
-}
-let img = window.document.querySelector(".imagem").style.backgroundImage
 
-img.style.backgroundImage="url(/img/atardecer.jpg)"
+
+
+
