a7d01e6b4d368e3a25c15119ffbab433c5b2c52c
Eliascolin <eliascolina.c@gmail.com>
2023-03-27 20:27:46 -0300

    modo responsivo 490px

--- script/script.js
+++ script/script.js
@@ -24,6 +24,12 @@ document.body.style.background = '#223a56'}
 }
 
 
+let nome = window.prompt("qual é teu nome")
 
+let inicio = window.alert(`Bem-Vindo ${nome} neste trabalho podes ver como aplico os conhecimentos de javascript.  alem de aprecentar a hora do dia, a imagem mudara de acordo com a hora do dia e a cor do body `)
+
+let bem = window.document.querySelector(".header_nome")
+
+bem.innerHTML = ` Bem-vindo ${nome}`
 
 
